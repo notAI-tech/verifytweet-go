bc7e267bd0cc96577b660a4cc7e6c70993a2c1f8
Preetham Kamidi <kamidipreetham@gmail.com>
2020-07-05 20:07:29 +0530

    WIP: Text Parsing
    Signed-off-by: Preetham Kamidi <kamidipreetham@gmail.com>

--- configs/configs.go
+++ configs/configs.go
@@ -1,1 +1,10 @@
 package configs
+
+// App represents application configuration
+type App struct {
+}
+
+// Init ...
+func Init() *App {
+	return &App{}
+}
--- internal/pkg/ocr/ocr.go
+++ internal/pkg/ocr/ocr.go
@@ -1,1 +1,52 @@
 package ocr
+
+import (
+	"github.com/otiai10/gosseract"
+	"gopkg.in/gographics/imagick.v3/imagick"
+)
+
+// Rescale ...
+func Rescale(image []byte) ([]byte, error) {
+	wand := imagick.NewMagickWand()
+	err := wand.ReadImageBlob(image)
+	if err != nil {
+		return nil, err
+	}
+	err = wand.ResampleImage(300, 300, imagick.FILTER_LAGRANGE)
+	if err != nil {
+		return nil, err
+	}
+	err = wand.SetImageAlphaChannel(imagick.ALPHA_CHANNEL_DEACTIVATE)
+	if err != nil {
+		return nil, err
+	}
+	err = wand.SetImageColorspace(imagick.COLORSPACE_GRAY)
+	if err != nil {
+		return nil, err
+	}
+	err = wand.BlurImage(1, 65000)
+	if err != nil {
+		return nil, err
+	}
+	err = wand.NormalizeImage()
+	if err != nil {
+		return nil, err
+	}
+	convertedImage := wand.GetImageBlob()
+	return convertedImage, nil
+}
+
+// ConvertToText ...
+func ConvertToText(image []byte) (string, error) {
+	client := gosseract.NewClient()
+	defer client.Close()
+	err := client.SetImageFromBytes(image)
+	if err != nil {
+		return "", err
+	}
+	text, err := client.Text()
+	if err != nil {
+		return "", err
+	}
+	return text, nil
+}
